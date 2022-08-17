e98d74b85b8d85d5262a07e3f0c4134ba8187570
kishorenarina <111413939+kishorenarina@users.noreply.github.com>
2022-08-17 18:48:44 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+  shared_config_files      =["/Users/tf_user/.aws/conf"]
+  shared_credentials_files = ["/Users/tf_user/.aws/creds"]
+  profile                  = "customprofile"
+}
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name = "single-instance"
+
+  ami                    = "ami-ebd02392"
+  instance_type          = "t2.micro"
+  key_name               = "user1"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-12345678"]
+  subnet_id              = "subnet-eddcdzz4"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
+
+
+
